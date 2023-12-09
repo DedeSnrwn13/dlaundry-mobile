0158d374ef721620693abdb121c6622f93dc0e0d
DedeSnrwn13 <sunarwandede13@gmail.com>
2023-12-09 19:20:50 +0700

    feat: dashboard page state

--- lib/pages/dashboard_page.dart
+++ lib/pages/dashboard_page.dart
@@ -1,6 +1,9 @@
 import 'package:dlaundry_mobile/config/app_constants.dart';
+import 'package:dlaundry_mobile/providers/dashboard_provider.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:d_view/d_view.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class DashboardPage extends StatelessWidget {
   const DashboardPage({super.key});
@@ -8,30 +11,46 @@ class DashboardPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: AppConstants.navMenuDashboard[0]['view'] as Widget,
+      body: Consumer(
+        builder: (_, wiRef, __) {
+          int navIndex = wiRef.watch(dashboardNavIndexProvider);
+
+          return AppConstants.navMenuDashboard[navIndex]['view'] as Widget;
+        },
+      ),
       extendBody: true,
       bottomNavigationBar: Padding(
         padding: const EdgeInsets.fromLTRB(70, 0, 70, 20),
-        child: Material(
-          borderRadius: BorderRadius.circular(10),
-          color: Colors.white,
-          child: BottomNavigationBar(
-            elevation: 0,
-            backgroundColor: Colors.transparent,
-            iconSize: 30,
-            type: BottomNavigationBarType.fixed,
-            onTap: (value) {},
-            showSelectedLabels: false,
-            showUnselectedLabels: false,
-            unselectedItemColor: Colors.grey[400],
-            items: AppConstants.navMenuDashboard.map((e) {
-              return BottomNavigationBarItem(
-                icon: Icon(e['icon']),
-                label: e['label'],
-              );
-            }).toList(),
-          ),
-        ),
+        child: Consumer(builder: (_, wiRef, __) {
+          int navIndex = wiRef.watch(dashboardNavIndexProvider);
+
+          if (kDebugMode) print(navIndex);
+
+          return Material(
+            borderRadius: BorderRadius.circular(10),
+            color: Colors.white,
+            elevation: 8,
+            child: BottomNavigationBar(
+              currentIndex: navIndex,
+              elevation: 0,
+              backgroundColor: Colors.transparent,
+              iconSize: 30,
+              type: BottomNavigationBarType.fixed,
+              onTap: (value) {
+                wiRef.read(dashboardNavIndexProvider.notifier).state = value;
+              },
+              showSelectedLabels: false,
+              showUnselectedLabels: false,
+              unselectedItemColor: Colors.grey[400],
+              items: AppConstants.navMenuDashboard.map((e) {
+                return BottomNavigationBarItem(
+                  icon: Icon(e['icon']),
+                  label: e['label'],
+                );
+              }).toList(),
+            ),
+          );
+        }),
       ),
     );
   }
