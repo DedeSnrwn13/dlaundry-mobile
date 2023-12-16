1c70a0b4174ab88aaf8261cdafba47b9b5ce6beb
DedeSnrwn13 <sunarwandede13@gmail.com>
2023-12-16 15:17:18 +0700

    feat: home view state

--- lib/pages/dashboard_views/home_view.dart
+++ lib/pages/dashboard_views/home_view.dart
@@ -3,6 +3,7 @@ import 'package:dlaundry_mobile/datasources/promo_datasource.dart';
 import 'package:dlaundry_mobile/datasources/shop_datasource.dart';
 import 'package:dlaundry_mobile/models/laundry_model.dart';
 import 'package:dlaundry_mobile/models/shop_model.dart';
+import 'package:dlaundry_mobile/providers/home_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -20,29 +21,33 @@ class _HomeViewState extends ConsumerState<HomeView> {
         (failure) {
           switch (failure.runtimeType) {
             case ServerFailure:
-              // setHomePromoStatus(ref, 'Server Error');
+              setHomePromoStatus(ref, 'Server Error');
               break;
             case NotFoundFailure:
-              // setHomePromoStatus(ref, 'Error Not Found');
+              setHomePromoStatus(ref, 'Error Not Found');
               break;
             case ForbiddenFailure:
-              // setHomePromoStatus(ref, 'You don\'t have access');
+              setHomePromoStatus(ref, 'You don\'t have access');
               break;
             case BadRequestFailure:
-              // setHomePromoStatus(ref, 'Bad Request');
+              setHomePromoStatus(ref, 'Bad Request');
               break;
             case UnauthorisedFailure:
-              // setHomePromoStatus(ref, 'Unauthorised');
+              setHomePromoStatus(ref, 'Unauthorised');
               break;
             default:
-              // setHomePromoStatus(ref, 'Request Error');
+              setHomePromoStatus(ref, 'Request Error');
               break;
           }
         },
         (result) {
+          setHomePromoStatus(ref, 'Success');
+
           List data = result['data'];
           List<PromoModel> promos =
               data.map((e) => PromoModel.fromJson(e)).toList();
+
+          ref.read(homePromoListProvider.notifier).setData(promos);
         },
       );
     });
@@ -54,32 +59,44 @@ class _HomeViewState extends ConsumerState<HomeView> {
         (failure) {
           switch (failure.runtimeType) {
             case ServerFailure:
-              // setHomePromoStatus(ref, 'Server Error');
+              setHomeRecommendationStatus(ref, 'Server Error');
               break;
             case NotFoundFailure:
-              // setHomePromoStatus(ref, 'Error Not Found');
+              setHomeRecommendationStatus(ref, 'Error Not Found');
               break;
             case ForbiddenFailure:
-              // setHomePromoStatus(ref, 'You don\'t have access');
+              setHomeRecommendationStatus(ref, 'You don\'t have access');
               break;
             case BadRequestFailure:
-              // setHomePromoStatus(ref, 'Bad Request');
+              setHomeRecommendationStatus(ref, 'Bad Request');
               break;
             case UnauthorisedFailure:
-              // setHomePromoStatus(ref, 'Unauthorised');
+              setHomeRecommendationStatus(ref, 'Unauthorised');
               break;
             default:
-              // setHomePromoStatus(ref, 'Request Error');
+              setHomeRecommendationStatus(ref, 'Request Error');
               break;
           }
         },
         (result) {
+          setHomeRecommendationStatus(ref, 'Success');
+
           List data = result['data'];
           List<ShopModel> shops =
               data.map((e) => ShopModel.fromJson(e)).toList();
+
+          ref.read(homeRecommendationListProvider.notifier).setData(shops);
         },
       );
     });
+  }
+
+  @override
+  void initState() {
+    getPromo();
+    getRecommendation();
+
+    super.initState();
   }
 
   @override
