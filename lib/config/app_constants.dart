bf63cfc46ae530f6d34f8f31d04d6bc078681d79
DedeSnrwn13 <sunarwandede13@gmail.com>
2024-01-06 09:35:43 +0700

    fix: account view file name

--- lib/config/app_constants.dart
+++ lib/config/app_constants.dart
@@ -1,28 +1,18 @@
 import 'package:d_view/d_view.dart';
-import 'package:dlaundry_mobile/pages/dashboard_views/AccountView.dart';
+import 'package:dlaundry_mobile/pages/dashboard_views/account_view.dart';
 import 'package:dlaundry_mobile/pages/dashboard_views/home_view.dart';
 import 'package:flutter/material.dart';
 
 class AppConstants {
   static const appName = 'D Laundry';
 
-  static const _host = 'http://127.0.0.1:8000';
+  static const _host = 'http://127.0.1:8000';
 
   static const baseURL = '$_host/api';
 
   static const baseImageURL = '$_host/storage';
 
-  static const laundryStatusCategory = [
-    'All',
-    'Pickup',
-    'Queue',
-    'Process',
-    'Washing',
-    'Dried',
-    'Ironed',
-    'Done',
-    'Delivery'
-  ];
+  static const laundryStatusCategory = ['All', 'Pickup', 'Queue', 'Process', 'Washing', 'Dried', 'Ironed', 'Done', 'Delivery'];
 
   static List<Map> navMenuDashboard = [
     {
@@ -42,11 +32,5 @@ class AppConstants {
     }
   ];
 
-  static const homeCategories = [
-    'All',
-    'Regular',
-    'Express',
-    'Economical',
-    'Exclusive'
-  ];
+  static const homeCategories = ['All', 'Regular', 'Express', 'Economical', 'Exclusive'];
 }
